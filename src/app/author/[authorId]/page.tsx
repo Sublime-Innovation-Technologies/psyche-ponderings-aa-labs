d2581df5d239101388ee4e45fd9221927a147a0f
Samarth MN <samarth.m.n@gmail.com>
2024-02-03 15:14:37 +0530

    init

---
+++ src/app/author/[authorId]/page.tsx
@@ -0,0 +1,9 @@
+import { AuthorPage } from "@/components/author-page";
+
+export default function Author() {
+  return (
+    <main>
+      <AuthorPage />
+    </main>
+  );
+}
---
+++ src/app/blogs/page.tsx
@@ -0,0 +1,9 @@
+import { BlogsList } from "@/components/blogs-list";
+
+export default function Blogs() {
+  return (
+    <main>
+      <BlogsList />
+    </main>
+  );
+}
